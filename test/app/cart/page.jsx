80a7d418f2b1ccb2796b7cc430d0683322d13957
PlatypusPus <145650588+PlatypusPus@users.noreply.github.com>
2024-07-13 17:16:47 +0530

    added some routes not for all

---
+++ test/app/cart/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div>
+        <header>
+            Cart
+        </header>
+      </div>
+      <div>
+        <p>
+            This where the user can see all the products he has added
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ test/app/login/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div>
+        <header>
+            Login
+        </header>
+      </div>
+      <div>
+        <p>
+            Page where the login signs up
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default page
